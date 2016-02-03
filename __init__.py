f42c10bb43c3a6a99d6caff54ec054d77141c2c5
ffunenga <fmafunenga@gmail.com>
2016-02-03 14:54:03 +0100

    Updates __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.0.1a0'
+__author__ = 'Filipe Funenga'
+__license__ = 'MIT'
+__description__ = ('A tool to swap the files on your repository\'s root '
+                   'directory with the ones inside your python package.')
+
+
+from main import main
