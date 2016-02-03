ab46db3eebc8bf52becee73641e68d9b973d6c98
ffunenga <fmafunenga@gmail.com>
2016-02-03 17:03:51 +0100

    Create new test for submodules become_implicit

---
+++ __ignore__/tests/test_become_implicit_submodules.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+import unittest
+import os
+import shutil
+
+import insideout
+
+
+def touch(path, content='(...)'):
+    with open(path, 'w') as f:
+        f.write(content)
+
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.__clean()
+        os.mkdir('temp')
+
+        touch('temp/README.md')
+        touch('temp/__init__.py')
+        touch('temp/main.py')
+        touch('temp/tools.py')
+
+        os.mkdir('temp/subpackage1')
+        touch('temp/subpackage1/__init__.py')
+        touch('temp/subpackage1/comms.py')
+
+        os.mkdir('temp/subpackage2')
+        touch('temp/subpackage2/__init__.py')
+        touch('temp/subpackage2/parsers.py')
+
+        os.mkdir('temp/__ignore__')
+        touch('temp/__ignore__/LICENSE.txt')
+        touch('temp/__ignore__/tox.ini')
+        content = "import setuptools\nsetuptools.setup(name='package')\n"
+        touch('temp/__ignore__/setup.py', content)
+        content = "package"
+        touch('temp/__ignore__/package_name.txt', content)
+
+        self.backup_cwd = os.getcwd()
+        os.chdir('temp')
+
+    def test_become_implicit(self):
+        #os.system('tree')
+        insideout.main([])
+        #os.system('tree')
+
+        files = os.listdir('package')
+        self.assertTrue('subpackage1' in sorted(files))
+        self.assertTrue('subpackage2' in sorted(files))
+
+        files = os.listdir('package/subpackage1')
+        self.assertTrue('__init__.py' in sorted(files))
+        self.assertTrue('comms.py' in sorted(files))
+
+        files = os.listdir('package/subpackage2')
+        self.assertTrue('__init__.py' in sorted(files))
+        self.assertTrue('parsers.py' in sorted(files))
+
+    def __clean(self):
+        try:
+            shutil.rmtree('temp')
+        except:
+            pass
+
+    def tearDown(self):
+        os.chdir(self.backup_cwd)
+        self.__clean()
