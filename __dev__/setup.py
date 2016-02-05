283611067460611aeaca77e71b303bdaa06945ec
ffunenga <fmafunenga@gmail.com>
2016-02-05 15:34:00 +0100

    Changes setup.py to new format

--- __dev__/setup.py
+++ __dev__/setup.py
@@ -1,59 +1,60 @@
 from __future__ import print_function
 import os
-import sys
 import shutil
-import getpass
+import sys
 import setuptools
 
-import insideout
+
+# Change the CWD to __dev__
+_t = os.path.abspath(__file__)
+cwd = os.path.dirname(_t)
+os.chdir(cwd)
+
+
+# Grabs package name from the root folder
+_t = os.path.join(cwd, '..')
+_t = os.path.abspath(_t)
+package = os.path.basename(_t)
 
 
 argv = sys.argv[1:]
-test_suite = os.environ.get('TOXTESTSUITE', 'tests')
+
+
+if 'clean' in argv:
+    shutil.rmtree(package, ignore_errors=True)
+    shutil.rmtree('%s.egg-info' % package, ignore_errors=True)
+    shutil.rmtree('build', ignore_errors=True)
+    shutil.rmtree('dist', ignore_errors=True)
+    #shutil.rmtree('env', ignore_errors=True)
+    #shutil.rmtree('.tox', ignore_errors=True)
+    sys.exit()
+
+
+# Replicates package inside __dev__
+shutil.rmtree(package, ignore_errors=True)
+ignore_list = shutil.ignore_patterns('__dev__*', '.git*')
+shutil.copytree('..', package, ignore=ignore_list)
+
+
+import insideout
 
 
 if 'test' in argv:
     pass
 
 
-if 'clean' in argv:
-    shutil.rmtree('%s.egg-info' % insideout.__name__, ignore_errors=True)
-    shutil.rmtree('build', ignore_errors=True)
-    shutil.rmtree('dist', ignore_errors=True)
-    shutil.rmtree('env', ignore_errors=True)
-    def pyclean(path):
-        for root, drs, fns in os.walk(path):
-            pycache = os.path.join(root, '__pycache__')
-            shutil.rmtree(pycache, ignore_errors=True)
-            filtered_fns = filter(lambda f: f.endswith('.pyc'), fns)
-            for fn in filtered_fns:
-                _fn = os.path.join(root, fn)
-                os.remove(_fn)
-    pyclean(insideout.__name__)
-    pyclean('tests')
-    shutil.rmtree('.tox', ignore_errors=True)
-
-
-long_description = """\
-A tool to develop ready-to-use python repositories.
-
-    Why aren't the files inside a Python package the first thing we see when we look at its github repository?
-
-Pure Python code (2 and 3 compatible).
-"""
-
-
 setuptools.setup(
     name=insideout.__name__,
     description=insideout.__description__,
-    long_description=long_description,
+    long_description=insideout.__lead__,
     version=insideout.__version__,
     author='Filipe Funenga',
     author_email='fmafunenga@gmail.com',
     url='https://github.com/ffunenga/%s' % insideout.__name__,
     license=insideout.__license__,
     packages=[insideout.__name__],
-    test_suite = test_suite,
+    package_dir={'': '..'},
+    test_suite = os.environ.get('TOXTESTSUITE', 'tests'),
     entry_points = {
         'console_scripts' : [
             '{pkg} = {pkg}:main'.format(pkg=insideout.__name__)
--- __init__.py
+++ __init__.py
@@ -1,8 +1,13 @@
 __version__ = '0.0.2a0'
 __author__ = 'Filipe Funenga'
 __license__ = 'MIT'
-__description__ = ('A tool to swap the files on your repository\'s root '
-                   'directory with the ones inside your python package.')
+__description__ = 'A tool to develop ready-to-use python repositories.'
+__lead__ = """\
+A tool to develop ready-to-use python repositories.
 
+    Why aren't the files inside a Python package the first thing we see when we look at its github repository?
+
+Pure Python code (2 and 3 compatible).
+"""
 
 from .main import main
