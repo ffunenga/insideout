117bd80e6a3dc96e720ee84e27d81b5beb73a2da
ffunenga <fmafunenga@gmail.com>
2016-02-06 01:21:38 +0100

    Updates setup.py to work correctly from any directory

--- __dev__/setup.py
+++ __dev__/setup.py
@@ -5,59 +5,54 @@ import sys
 import setuptools
 
 
-# Change the CWD to __dev__
+PACKAGE_NAME = 'insideout'
+
+
 _t = os.path.abspath(__file__)
 cwd = os.path.dirname(_t)
-os.chdir(cwd)
+if os.path.basename(cwd) == '__dev__':
+    # Changes CWD to __dev__
+    os.chdir(cwd)
+    if 'clean' in sys.argv[1:]:
+        shutil.rmtree(PACKAGE_NAME, ignore_errors=True)
+        shutil.rmtree('%s.egg-info' % PACKAGE_NAME, ignore_errors=True)
+        shutil.rmtree('build', ignore_errors=True)
+        shutil.rmtree('dist', ignore_errors=True)
+        shutil.rmtree('env', ignore_errors=True)
+        shutil.rmtree(os.path.join('..', 'env'), ignore_errors=True)
+        shutil.rmtree('.tox', ignore_errors=True)
+        shutil.rmtree(os.path.join('..', '.tox'), ignore_errors=True)
+        for root, drs, fns in os.walk('tests'):
+            pycache = os.path.join(root, '__pycache__')
+            shutil.rmtree(pycache, ignore_errors=True)
+            filtered_fns = filter(lambda f: f.endswith('.pyc'), fns)
+            for fn in filtered_fns:
+                _fn = os.path.join(root, fn)
+                os.remove(_fn)
+        sys.exit()
+    # Replicates package inside __dev__
+    shutil.rmtree(PACKAGE_NAME, ignore_errors=True)
+    ignore_list = shutil.ignore_patterns('__dev__*', '.git*', 'env*', '.tox')
+    shutil.copytree('..', PACKAGE_NAME, ignore=ignore_list)
 
 
-# Grabs package name from the root folder
-_t = os.path.join(cwd, '..')
-_t = os.path.abspath(_t)
-package = os.path.basename(_t)
-
-
-argv = sys.argv[1:]
-
-
-if 'clean' in argv:
-    shutil.rmtree(package, ignore_errors=True)
-    shutil.rmtree('%s.egg-info' % package, ignore_errors=True)
-    shutil.rmtree('build', ignore_errors=True)
-    shutil.rmtree('dist', ignore_errors=True)
-    #shutil.rmtree('env', ignore_errors=True)
-    #shutil.rmtree('.tox', ignore_errors=True)
-    sys.exit()
-
-
-# Replicates package inside __dev__
-shutil.rmtree(package, ignore_errors=True)
-ignore_list = shutil.ignore_patterns('__dev__*', '.git*')
-shutil.copytree('..', package, ignore=ignore_list)
-
-
-import insideout
-
-
-if 'test' in argv:
-    pass
+package = __import__(PACKAGE_NAME)
 
 
 setuptools.setup(
-    name=insideout.__name__,
-    description=insideout.__description__,
-    long_description=insideout.__lead__,
-    version=insideout.__version__,
+    name=package.__name__,
+    description=package.__description__,
+    long_description=package.__lead__,
+    version=package.__version__,
     author='Filipe Funenga',
     author_email='fmafunenga@gmail.com',
-    url='https://github.com/ffunenga/%s' % insideout.__name__,
-    license=insideout.__license__,
-    packages=[insideout.__name__],
-    package_dir={'': '..'},
+    url='https://github.com/ffunenga/%s' % package.__name__,
+    license=package.__license__,
+    packages=[package.__name__],
     test_suite = os.environ.get('TOXTESTSUITE', 'tests'),
     entry_points = {
         'console_scripts' : [
-            '{pkg} = {pkg}:main'.format(pkg=insideout.__name__)
+            '{pkg} = {pkg}:main'.format(pkg=package.__name__)
         ]
     }
 )
