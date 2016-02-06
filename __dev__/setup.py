a0c5a3ba3ac4e8cac729f67d98e1ad9ddaa9b95b
ffunenga <fmafunenga@gmail.com>
2016-02-06 01:50:09 +0100

    Improves readability of setup.py

--- __dev__/setup.py
+++ __dev__/setup.py
@@ -8,32 +8,57 @@ import setuptools
 PACKAGE_NAME = 'insideout'
 
 
+# ---------------------------------------------------------------------------- #
+#
+# The following segment of code enables this setup.py to work as expected
+# from anywhere.
+#
+# From the root of the repository you can run these:
+#     $ python __dev__/setup.py install
+#     $ tox -c __dev__/tox.ini
+#
+# From inside __dev__ you can run these:
+#     $ python setup.py install
+#     $ tox
+#
+# If you organize your code in the classical testing/deployment focused
+# structure (where all the test configuration files, license, etc., are placed
+# in the root directory of your repository) then this setup.py also works.
+# Although, you shouldn't do it. Just look at it: it's ugly.
+#
+# --- SEGMENT OF CODE -------------------------------------------------------- #
+
 _t = os.path.abspath(__file__)
 cwd = os.path.dirname(_t)
+
 if os.path.basename(cwd) == '__dev__':
-    # Changes CWD to __dev__
+
     os.chdir(cwd)
-    if 'clean' in sys.argv[1:]:
-        shutil.rmtree(PACKAGE_NAME, ignore_errors=True)
-        shutil.rmtree('%s.egg-info' % PACKAGE_NAME, ignore_errors=True)
-        shutil.rmtree('build', ignore_errors=True)
-        shutil.rmtree('dist', ignore_errors=True)
-        shutil.rmtree('env', ignore_errors=True)
-        shutil.rmtree(os.path.join('..', 'env'), ignore_errors=True)
-        shutil.rmtree('.tox', ignore_errors=True)
-        shutil.rmtree(os.path.join('..', '.tox'), ignore_errors=True)
-        for root, drs, fns in os.walk('tests'):
-            pycache = os.path.join(root, '__pycache__')
-            shutil.rmtree(pycache, ignore_errors=True)
-            filtered_fns = filter(lambda f: f.endswith('.pyc'), fns)
-            for fn in filtered_fns:
-                _fn = os.path.join(root, fn)
-                os.remove(_fn)
-        sys.exit()
-    # Replicates package inside __dev__
+
     shutil.rmtree(PACKAGE_NAME, ignore_errors=True)
     ignore_list = shutil.ignore_patterns('__dev__*', '.git*', 'env*', '.tox')
     shutil.copytree('..', PACKAGE_NAME, ignore=ignore_list)
+
+# --- END OF SEGMENT OF CODE ------------------------------------------------- #
+
+
+if 'clean' in sys.argv[1:]:
+    shutil.rmtree(PACKAGE_NAME, ignore_errors=True)
+    shutil.rmtree('%s.egg-info' % PACKAGE_NAME, ignore_errors=True)
+    shutil.rmtree('build', ignore_errors=True)
+    shutil.rmtree('dist', ignore_errors=True)
+    shutil.rmtree('env', ignore_errors=True)
+    shutil.rmtree(os.path.join('..', 'env'), ignore_errors=True)
+    shutil.rmtree('.tox', ignore_errors=True)
+    shutil.rmtree(os.path.join('..', '.tox'), ignore_errors=True)
+    for root, drs, fns in os.walk('tests'):
+        pycache = os.path.join(root, '__pycache__')
+        shutil.rmtree(pycache, ignore_errors=True)
+        filtered_fns = filter(lambda f: f.endswith('.pyc'), fns)
+        for fn in filtered_fns:
+            _fn = os.path.join(root, fn)
+            os.remove(_fn)
+    sys.exit()
 
 
 package = __import__(PACKAGE_NAME)
@@ -41,8 +66,8 @@ package = __import__(PACKAGE_NAME)
 
 setuptools.setup(
     name=package.__name__,
-    description=package.__description__,
-    long_description=package.__lead__,
+    description=package.__lead__,
+    long_description=package.__description__,
     version=package.__version__,
     author='Filipe Funenga',
     author_email='fmafunenga@gmail.com',
--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 __version__ = '0.0.2a0'
 __author__ = 'Filipe Funenga'
 __license__ = 'MIT'
-__description__ = 'A tool to develop ready-to-use python repositories.'
-__lead__ = """\
+__lead__ = 'A tool to develop ready-to-use python repositories.'
+__description__ = """\
 A tool to develop ready-to-use python repositories.
 
     Why aren't the files inside a Python package the first thing we see when we look at its github repository?
